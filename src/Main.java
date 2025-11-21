c6d0978111008a81b3cb37764acd8fa083188f3b
IGITANGAZA23 <igitangazanobleprince@gmail.com>
2025-11-21 09:38:15 -0800

    feat: add Lamda expression

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,44 @@
 public class Main {
     public static void main(String[] args) {
 
-        Calculator calculator = new Calculator();
+        /*Calculator calculator = new Calculator();
 
-        // Create tasks
+
         Runnable task1 = new CalculationTask(calculator, 10, 5, "add");
         Runnable task2 = new CalculationTask(calculator, 20, 3, "subtract");
         Runnable task3 = new CalculationTask(calculator, 6, 7, "multiply");
         Runnable task4 = new CalculationTask(calculator, 50, 10, "divide");
 
-        // Assign Runnable tasks to Threads
+
         Thread t1 = new Thread(task1, "Thread-A");
         Thread t2 = new Thread(task2, "Thread-B");
         Thread t3 = new Thread(task3, "Thread-C");
         Thread t4 = new Thread(task4, "Thread-D");
 
-        // Start threads
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();*/
+
+        Calculator calc = new Calculator();
+
+        Thread t1 = new Thread(() -> {
+            System.out.println("ADD Result: " + calc.add(10, 5));
+        });
+
+        Thread t2 = new Thread(() -> {
+            System.out.println("SUBTRACT Result: " + calc.subtract(20, 3));
+        });
+
+        Thread t3 = new Thread(() -> {
+            System.out.println("MULTIPLY Result: " + calc.multiply(6, 7));
+        });
+
+        Thread t4 = new Thread(() -> {
+            System.out.println("DIVIDE Result: " + calc.divide(50, 10));
+        });
+
+        // Start all threads
         t1.start();
         t2.start();
         t3.start();
